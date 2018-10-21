be7eacb03e0efbe246af9bd5bfa5c77ad8b92f13
Sean <seanpitfield@gmail.com>
2018-10-20 23:48:52 -0700

    almost2

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,9 @@
 //Global Variables
 
 var time = 20;
+var correctAnswers = 0;
+var wrongAnswers = 0;
+var intervalId;
 var questions = [
     {
         type: "Which one is not an object oriented programming language?",
@@ -25,76 +28,107 @@ var questions = [
     }
 ];
 
-var startBtn = document.getElementById('start-button');
-// Ready
-//debugger;
-let quizArea = document.getElementById('questions');
-quizArea.style.display = 'none';
+// var startBtn = document.getElementById('start-button');
+// // Ready
+// //debugger;
 
-//
-let buttonArea = document.getElementsByClassName('buttons');
-buttonArea.style.display = 'none';
+// let quizArea = document.getElementById('questions');
+// quizArea.style.display = 'none';
+
+// //
+// let buttonArea = document.getElementsByClassName('buttons');
+// buttonArea.style.display = 'none';
 
 // Start Function
-$(document).ready(function() {
+var startGame= function() {
 
+    $(".buttons").empty();
     //when start-button is clicked...
     $("#start-button").on("click", function() {
         // quizArea.style.display = "show";
         // buttonArea.style.display = "show";
     //...create a string that will hold the question.
-    var questionNumber = [];
+    var questions = [
+        {
+            type: "Which one is not an object oriented programming language?",
+            choices: ["Java", "C#", "C++", "C"],
+            answer: "C"
+        }, {
+            type: "Which language is used for styling web pages?",
+            choices: ["HTML", "JQuery", "CSS", "XML"],
+            answer: "CSS"
+        }, {
+            type: "There are ___ main components of object oriented programming?",
+            choices: ["1", "6", "2", "4"],
+            answer: "4"
+        }, {
+            type: "Which language is used for web apps?",
+            choices: ["PHP", "Python", "Javascript", "All"],
+            answer: "All"
+        }, {
+            type: "MVC is a _______.",
+            choices: ["Language", "Library", "Framework", "All"],
+            answer: "Framework"
+        }
+    ];
 
     //Now a loop that generates the question
         for (let i = 0; i < questions.length; i++) {
         
-        document.write(questions[i].type);
+       // document.write(questions[i].type);
 
-        var random = $('#questions').append("<p>" +questions[i].type + "</p>");
+        var randomQ = $('#questions').append("<p>" +questions[i].type + "</p>");
 
-        questionNumber = random + questionNumber;
-
-        $('.buttons').append("<button>" +questions[i].choices + "</button>");
+        // questions = random + questionNumber;
+            var button =$("<div>");
+                button.attr({
+                    "class":'btn',
+                    "data":randomQ
+                });
+         
     
     }
 
 
 
 })
+};
+
+startGame();
+
+$(document).on('click', ".buttons", function (){
+
+    var choiceQ = $('.buttons').append("<button>" +questions[i].choices + "</button>");
+
+    correctAnswers += choiceQ;
+
+    $("#choice").html("Total Score: " + correctAnswers);
+
+    console.log(previous);
+
+    
+    if(correctAnswers > random_result){
+
+        lost++;
+
+        $("#lost").html("You Lost: " + lost);
+
+        previous = 0;
+
+        startGame();
+    }
+    
+    else if(previous === random_result){
+
+        win++;
+
+        $("#win").html("You Win: " + win);
+
+        previous = 0;
+
+        startGame();
+    }
+
+    
 });
 
-
-
-
-
-
-
-
-
-
-var correctAnswers;
-var wrongAnswers;
-var intervalId;
-
-//Functions
-//test
-console.log(questions.q2)
-
-function startGame(){
-
-}
-
-function gamePlay(){
-
-    $('.startBtn').on('click',function()
-    {
-
-    $('button').remove('.startBtn');
-});
-}
-
-// function quest1() {
-
-//     var newDiv = $('<div class=" quest q1"/>');
-//     newDiv.html('testing');
-// }
